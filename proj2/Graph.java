4a6f054ce95e052cad70af49a6442fd2338ed928
KaiJun27 <73013627+KaiJun27@users.noreply.github.com>
2020-10-26 22:13:22 +0800

    Add files via upload

--- proj2/Graph.java
+++ proj2/Graph.java
@@ -7,30 +7,34 @@ public class Graph {
 	private int n;
 	private LinkedList<Integer> adjList[];
 	
-	public Graph(int numOfNodes) {
+	//Graph Constructor
+	public Graph(int numOfNodes) { 
 		this.n = numOfNodes;
 		adjList = new LinkedList[n];
 		for(int i= 0; i < n; i++) {
-			adjList[i] = new LinkedList();
+			adjList[i] = new LinkedList(); //Each node will have their own linked list 
 		}
 	}
+	
 	
 	public int getSize() {
 		return n;
 	}
 	
+	//method to add edges
 	public void addEdge(int fromNode, int toNode) {
 		adjList[fromNode].add(toNode);
 		adjList[toNode].add(fromNode);
 		
 	}
 
-	
+	//returns number of edges of a node
 	public int findDegree(int nodeID) {
 
 		return adjList[nodeID].size();
 	}
 	
+	//returns all children of a node in an array
 	public int[] findChild(int node) {
 		int size = findDegree(node);
 		int[] childs = new int[size];
