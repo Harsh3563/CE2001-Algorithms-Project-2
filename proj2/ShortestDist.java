ee1d16b159e4963ec9b9547227fb5e7bec13e070
zherenlee <73013641+zherenlee@users.noreply.github.com>
2020-10-26 19:39:02 +0800

    Add files via upload

--- proj2/ShortestDist.java
+++ proj2/ShortestDist.java
@@ -7,16 +7,17 @@ public class ShortestDist {
 		LinkedList<Integer> L = new LinkedList<Integer>();
 		L.add(s);
 		boolean visited[] = new boolean[nodes];
-		int dist = 0;
+		//int dist = 0;
 		int u;
 		int[] children;
 		int j, count = 0;
-		int visitCount;
+		//int visitCount;
 		int[] distances = new int[nodes];
+		int[] pre = new int[nodes];
 		int[] hosp = new int[k];
 		for (j = 0; j<h.length; j++) {
         	if (s == h[j]) {
-        		distances[count] = dist;
+        		distances[count] = 0;
         		count++;
         		if(count == k) {
 					System.out.print("Distances are: ");
@@ -28,18 +29,19 @@ public class ShortestDist {
 				}
         	}
         }
-		dist++;
+		//dist++;
 		
 		for (int i = 0; i < nodes; i++) {
 			visited[i] = false;
-			//dist[i] = Integer.MAX_VALUE;
+			//distances[i] = Integer.MAX_VALUE;
+			pre[i] = -1;
 		}
 		
 		visited[s] = true;
 		distances[s] = 0;
 		while (!L.isEmpty()) { 
             u = L.remove();
-            visitCount = 0;
+            //visitCount = 0;
             children = g.findChild(u);
             
 	    for (int i = 0; i < g.findDegree(u); i++) { 
@@ -47,11 +49,11 @@ public class ShortestDist {
 		if (visited[children[i]] == false) { 
 		    visited[children[i]] = true; 
 		    distances[children[i]] = distances[u] + 1;
+		    pre[children[i]] = u;
 		    L.add(children[i]); 
 
 
-		    // stopping condition (when we find 
-		    // our destination) 
+		    // stopping condition
 	    for (j = 0; j<h.length; j++) {
 				if (children[i] == h[j]){ 
 					hosp[count] = distances[children[i]];
@@ -62,15 +64,29 @@ public class ShortestDist {
 							System.out.print(hosp[z]+ " ");
 						}
 						System.out.println();
+						LinkedList<Integer> path = new LinkedList<Integer>();
+						for (int q=0; q<hosp.length; q++) {
+							int trace = hosp[q];
+							path.add(trace);
+							while (pre[trace] != -1) {
+								path.add(pre[trace]);
+								trace = pre[trace];
+							}
+							System.out.print("Path: "); 
+					        for (int w = path.size() - 1; w >= 0; w--) { 
+					            System.out.print(path.get(w) + " "); 
+					        } 
+					        System.out.println();
+						}
 						return;
 					}
 			    }
 			}
 		}
-		else visitCount++;
+		//else visitCount++;
 	    } 
-	    if(g.findDegree(u) > visitCount)
-	    	dist++;
+	    //if(g.findDegree(u) > visitCount)
+	    //	dist++;
         }
 		if(count == 0) {
 			System.out.println("No hospitals connected to source node");
@@ -82,6 +98,20 @@ public class ShortestDist {
 				System.out.print(hosp[z]+ " ");
 			}
 			System.out.println();
+			LinkedList<Integer> path = new LinkedList<Integer>(); 
+			for (int q=0; q<hosp.length; q++) {
+				int trace = hosp[q];
+				path.add(trace);
+				while (pre[trace] != -1) {
+					path.add(pre[trace]);
+					trace = pre[trace];
+				}
+				System.out.print("Path: "); 
+		        for (int i = path.size() - 1; i >= 0; i--) { 
+		            System.out.print(path.get(i) + " "); 
+		        }
+		        System.out.println();
+			}
 		}
 	}
 }
