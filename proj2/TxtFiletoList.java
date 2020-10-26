4e5d3ec334dc0b8bc2413e9ddee9fa51619ff4dc
Harsh Doddannavar <55326577+Harsh3563@users.noreply.github.com>
2020-10-26 22:29:05 +0800

    Update TxtFiletoList.java

--- proj2/TxtFiletoList.java
+++ proj2/TxtFiletoList.java
@@ -18,7 +18,7 @@ public class TxtFiletoList
        arr = list.toArray(arr); // string array stores the values in the array list as string variables
        int[] arrFinal = new int[arr.length-2]; // integer array to store the hospitals' node IDs in the string array as integers
        int k=0;
-       for(int i=2; i<arr.length; i++) // for-loop ignores the 1st value of the string array, which is essentially the total no. of hospitals (since 'arrFinal' only stores the hospitals' node IDs) 
+       for(int i=2; i<arr.length; i++) // for-loop ignores the 1st value of the string array, which is essentially the hashtag ('#') and the total no. of hospitals (since 'arrFinal' only stores the hospitals' node IDs) 
        {       
            arrFinal[k] = Integer.parseInt(arr[i]);
            k++;
